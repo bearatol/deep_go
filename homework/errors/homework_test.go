a6c378e4f31092127423424176b80f1e2c95c118
Diesperov Mikhail <bearatol@yandex.ru>
2025-05-30 13:48:40 +0300

    feat: added errors task

---
+++ homework/errors/homework_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// go test -v homework_test.go
+
+type MultiError struct {
+	Errors []error
+}
+
+func (e *MultiError) Error() string {
+	str := fmt.Sprintf("%d errors occured:\n", len(e.Errors))
+	for _, v := range e.Errors {
+		str += "\t* " + v.Error()
+	}
+	return str + "\n"
+}
+
+func Append(err error, errs ...error) *MultiError {
+	mErr, ok := err.(*MultiError)
+	if !ok {
+		mErr = &MultiError{}
+	}
+	mErr.Errors = append(mErr.Errors, errs...)
+	return mErr
+}
+
+func TestMultiError(t *testing.T) {
+	var err error
+	err = Append(err, errors.New("error 1"))
+	err = Append(err, errors.New("error 2"))
+
+	expectedMessage := "2 errors occured:\n\t* error 1\t* error 2\n"
+	assert.EqualError(t, err, expectedMessage)
+}
