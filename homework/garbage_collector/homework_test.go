738a41ed150de992b3bac3921aa8db3ce52c27f1
Diesperov Mikhail <bearatol@yandex.ru>
2025-06-08 21:25:57 +0300

    feat: added garbage_collector task

---
+++ homework/garbage_collector/homework_test.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+	"unsafe"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// go test -v homework_test.go
+
+func Trace(stacks [][]uintptr) []uintptr {
+
+	var (
+		stack, result []uintptr
+		visited       = make(map[uintptr]struct{})
+	)
+
+	for i := len(stacks) - 1; i >= 0; i-- {
+		for j := len(stacks[i]) - 1; j >= 0; j-- {
+			ptr := stacks[i][j]
+			if ptr != 0 {
+				if _, exists := visited[ptr]; !exists {
+					stack = append(stack, ptr)
+					visited[ptr] = struct{}{}
+				}
+			}
+		}
+	}
+
+	for len(stack) > 0 {
+		currentPtr := stack[len(stack)-1]
+		result = append(result, currentPtr)
+		stack = stack[:len(stack)-1]
+
+		ptrValue := *(*uintptr)(unsafe.Pointer(currentPtr))
+		if ptrValue == 0 {
+			continue
+		}
+		if _, exists := visited[ptrValue]; exists {
+			continue
+		}
+
+		stack = append(stack, ptrValue)
+		visited[ptrValue] = struct{}{}
+	}
+
+	return result
+}
+
+func TestTrace(t *testing.T) {
+	var heapObjects = []int{
+		0x00, 0x00, 0x00, 0x00, 0x00,
+	}
+
+	var heapPointer1 *int = &heapObjects[1]
+	var heapPointer2 *int = &heapObjects[2]
+	var heapPointer3 *int = nil
+	var heapPointer4 **int = &heapPointer3
+
+	var stacks = [][]uintptr{
+		{
+			uintptr(unsafe.Pointer(&heapPointer1)), 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, uintptr(unsafe.Pointer(&heapObjects[0])),
+			0x00, 0x00, 0x00, 0x00,
+		},
+		{
+			uintptr(unsafe.Pointer(&heapPointer2)), 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, uintptr(unsafe.Pointer(&heapObjects[1])),
+			0x00, 0x00, 0x00, uintptr(unsafe.Pointer(&heapObjects[2])),
+			uintptr(unsafe.Pointer(&heapPointer4)), 0x00, 0x00, 0x00,
+		},
+		{
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, uintptr(unsafe.Pointer(&heapObjects[3])),
+		},
+	}
+
+	pointers := Trace(stacks)
+	expectedPointers := []uintptr{
+		uintptr(unsafe.Pointer(&heapPointer1)),
+		uintptr(unsafe.Pointer(&heapObjects[0])),
+		uintptr(unsafe.Pointer(&heapPointer2)),
+		uintptr(unsafe.Pointer(&heapObjects[1])),
+		uintptr(unsafe.Pointer(&heapObjects[2])),
+		uintptr(unsafe.Pointer(&heapPointer4)),
+		uintptr(unsafe.Pointer(&heapPointer3)),
+		uintptr(unsafe.Pointer(&heapObjects[3])),
+	}
+
+	assert.True(t, reflect.DeepEqual(expectedPointers, pointers))
+}
